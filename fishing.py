ee584aebc76c74651d5c91b9b7d5621a2ed0d15a
gnwks <121499392+not-so-lonely@users.noreply.github.com>
2023-03-28 20:24:49 +0500

    Update fishing.py

--- fishing.py
+++ fishing.py
@@ -9,3 +9,9 @@ okno = display.set_mode((600, 1000))
 
 
 
+fon1 = image.load("bg.jpg") 
+fon2 = image.load("bg.jpg") 
+fon3 = image.load("bg.jpg")
+fon1 = transform.scale(fon1, (600, 1000)) 
+fon2 = transform.scale(fon2, (600, 1000)) 
+fon3 = transform.scale(fon3, (600, 1000)) 
