d4584648632414a82bdcdda0994449a3273440e9
gnwks <121499392+not-so-lonely@users.noreply.github.com>
2023-03-28 20:01:11 +0500

    Update mff.py

--- mff.py
+++ mff.py
@@ -1,12 +1,1 @@
-a = input('введите число')
-prent('a', a)
-if a = 1:
-  say('хрю')
-  print('у меня нет прав')
- while a < 0:
-   b=tru
-   a=fols
-   while a:
-      print"it is end"
-      
-now check the programm :)
+
